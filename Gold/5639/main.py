8fa0199a56355a3d0ea6f28df0509154f4b4ccbc
junSung <not971006@gmail.com>
2026-01-31 01:04:40 +0900

    1월 31일 백준 5639번

---
+++ Gold/5639/main.py
@@ -0,0 +1,29 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+
+def to_tree(arry):
+  if len(arry) == 0:
+    return 0
+  
+  root = arry[0]
+  # ⚠️ 오른쪽 서브트리 시작점 찾기
+  idx = len(arry)
+
+  for i in range(1, len(arry)):
+    if root < arry[i]:
+      idx = i
+      break
+
+  left = arry[1:idx]
+  right = arry[idx:]
+
+  to_tree(left)
+  to_tree(right)
+  print(root)
+
+
+
+preorder = [int(x) for x in sys.stdin]
+
+to_tree(preorder)
